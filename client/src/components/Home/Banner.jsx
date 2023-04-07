d15a8d9a86f198fce6dc2ef9aa22b93982f11c08
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-04-07 22:13:56 +0530

    carousel
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ client/src/components/Home/Banner.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import { bannerData } from '../../constants/data';
+import styled from '@emotion/styled';
+
+const Image = styled('img') ({
+    width: '100%',
+    height: 280
+})
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+const Banner = () => {
+  return (
+    <Carousel 
+        swipeable={false}
+        draggable={false}
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={4000}
+        containerClass="carousel-container"
+        itemClass="carousel-item-padding-40-px"
+    >
+        {
+            bannerData.map(data => (
+                <Image src={data.url} alt="banner" />
+            ))
+        }
+    </Carousel>
+    )
+}
+
+export default Banner
