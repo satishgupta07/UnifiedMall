8652d7692a9936e20e90f124aa8add3ddbe64d92
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-08-20 18:53:40 +0530

    Implemented the search functionality
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- client/src/components/Header/Search.jsx
+++ client/src/components/Header/Search.jsx
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
-import { Box, InputBase } from '@mui/material';
+import { Box, InputBase, List, ListItem } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../redux/actions/productActions';
+import { Link } from 'react-router-dom';
 
 const SearchContainer = styled(Box)`
     background: #fff;
@@ -22,13 +25,56 @@ const SearchIconWrapper = styled(Box)`
     display: flex;
 `
 
+const ListWrapper = styled(List)`
+    position: absolute;
+    background: #FFFFFF;
+    color: #000;
+    margin-top: 36px;
+`
+
 const Search = () => {
+
+    const [text, setText] = useState('');
+
+    const { products } = useSelector(state => state.getProducts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [dispatch])
+
+    const getText = (text) => {
+        setText(text);
+    }
+
   return (
     <SearchContainer>
-        <InputSearchBase placeholder='Search for products, brands and more' />
+        <InputSearchBase 
+            placeholder='Search for products, brands and more'
+            onChange={(e) => getText(e.target.value)}
+            value={text}
+        />
         <SearchIconWrapper>
             <SearchIcon />
         </SearchIconWrapper>
+        {
+            text && 
+                <ListWrapper>
+                    {
+                        products.products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => (
+                            <ListItem>
+                                <Link 
+                                    to={`/product/${product.id}`}
+                                    onClick={() => setText('')}
+                                    style={{ textDecoration: 'none', color: 'inherit'}}
+                                >  
+                                    {product.title.longTitle}
+                                </Link>
+                            </ListItem>
+                        ))
+                    }
+                </ListWrapper>
+        }
     </SearchContainer>
   )
 }
