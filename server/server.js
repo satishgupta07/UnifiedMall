ce0bb218bd65f96754d622b53742de417d8e4734
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-11-26 23:45:44 +0530

    Vercel deployment
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Router);
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
