4616c1454013463a62b291587d130688adc19222
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-03 00:13:52 +0530

    Sync

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -14,7 +14,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST })
-        const { data } = await axios.get(`http://localhost:5000/product/${id}`);
+        const { data } = await axios.get(`https://unifiedmall-server.onrender.com/product/${id}`);
         dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
         dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_FAIL, payload: error.response });
